2fa7dbd59d67c6dbba4b1dc4dbf7db30697cd9c6
rounaksinghxd <singhrounak00326@gmail.com>
2026-01-24 00:04:51 +0530

    Learned Basics of C and its Structure

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # c-programming
 This is for C language.
+In this Repository you learn about C language from basics.
+
---
+++ basics.c
@@ -0,0 +1,14 @@
+//Basics of C Programming
+/*
+C Programming is also known as Mother of all Programming languages.
+Developed in the early 1970s by Dennis Ritchie at Bell Labs,
+it’s a middle-level language that balances the power of low-level assembly with the readability of high-level languages.
+*/
+
+//The Basic Structure of C
+#include<stdio.h>
+int main()
+{
+ printf("Hello World");
+ return 0;
+}
