05d6089cd0d8ef29036dce6a2db295dcd43ead3b
rounaksinghxd <singhrounak00326@gmail.com>
2026-01-24 11:30:06 +0530

    Take input from the user and print the output

--- identifiers.c
+++ identifiers.c
@@ -8,4 +8,13 @@ You cannot use C Keywords (reserved words) as identifiers.
 Keywords are words that already have a special meaning to the compiler, such as int, return, if, or while.
 
 */
+int first_n, second_n, result;
+printf("Enter the first number:");
+scanf("%d", &first_n);
+printf("Enter the second number:");
+scanf("%d", &second_n);
+result = first_n+second_n;
+printf("The Output of the additon is: %d", result);
+return 0;
+
 }
