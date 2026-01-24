4479038a4fef8ccdbefb9ba53e86b655ec096f14
rounaksinghxd <singhrounak00326@gmail.com>
2026-01-24 11:52:44 +0530

    Learned about Format Specifiers

--- identifiers.c
+++ identifiers.c
@@ -16,5 +16,24 @@ scanf("%d", &second_n);
 result = first_n+second_n;
 printf("The Output of the additon is: %d", result);
 return 0;
-
 }
+//In this code we use "%d" which is known as Format specifer.
+/*
+Format Specifer: In C, a Format Specifier is a placeholder used in input and output functions (like printf and scanf).
+It tells the compiler what type of data to expect and how to format it.
+Common Format Specifiers:
+Specifier               Data Type                        Example
+
+%d or %i                int (integer)                    10, -5
+%f                      float (decimal)                  3.14
+%lf                     double (Long decimal)            3.1415478955
+%c                      char (single character)          'A', '$'
+%s                      string (array of characters)     "Hello"
+%p                      Pointers (Memory Address)        0x7ffee                         
+
+> Why Format Specifers are Important?
+Computers store everything as binary ($0$s and $1$s).
+Without a format specifier, the computer wouldn't know if a specific chunk of memory represents the number 65,
+the character 'A', or a very small decimal. The specifier acts as a translator.
+
+*/
