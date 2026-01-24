e1b8b458bf7ab9c6a82df0605dd1bc9acd749d7e
rounaksinghxd <singhrounak00326@gmail.com>
2026-01-24 12:19:58 +0530

    Learned Arithmetic and Relational Operators

--- operators.c
+++ operators.c
@@ -6,7 +6,21 @@ Operators: Operators are used in programs to manipulates data and variables. The
 C operators can be classified into a number of categories. They include:
 
 1. Arithmetic Oprators
+  These are used for basic mathematical calculations.
+  Operator          Meaning               Example (a = 10, b =3)
+  +                 Addition               13
+  -                 Subtraction            7
+  *                 Multiplication         30
+  /                 Division               a/b=3 (integer division)
+  %                 Modulus (Remainder)    a%b = 1
+
 2. Relational Operators
+  These compare two values and return either true (1) or false (0).
+  They are essential for decision-making.
+  ==: Equal to (e.g., 5 == 5 is true)
+  !=: Not equal to
+  > and <: Greater than and Less than
+  >= and <=: Greater/Less than or equal to
 3. Logical Operators
 4. Assignment Operators
 5. Increment and Decrement Operators
