d0a382625f9b026af0cf228712d6cc65c1de9d35
rounaksinghxd <singhrounak00326@gmail.com>
2026-01-24 12:11:42 +0530

    Learned about Operators

---
+++ operators.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+/*
+Operators: Operators are used in programs to manipulates data and variables. They usually form a part of the mathematical or logical expressions.
+C operators can be classified into a number of categories. They include:
+
+1. Arithmetic Oprators
+2. Relational Operators
+3. Logical Operators
+4. Assignment Operators
+5. Increment and Decrement Operators
+6. Conditional Operators
+7. Bitwise Operators
+8. Special Operators
+
+*/
+
+}
