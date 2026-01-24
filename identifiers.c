f6583608e7fd249005d75de724adf58f5f42238e
rounaksinghxd <singhrounak00326@gmail.com>
2026-01-24 11:15:32 +0530

    Learned about Identifiers

---
+++ identifiers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+/*Identifiers allow us to name you give to entities like variables,
+functions, structures, or arrays. Think of it as a label that helps the compiler identify a specific memory location or a block of code. 
+
+You cannot use C Keywords (reserved words) as identifiers.
+Keywords are words that already have a special meaning to the compiler, such as int, return, if, or while.
+
+*/
+}
