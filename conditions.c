226264177613da7a23760fad02eddb32fc807178
rounaksinghxd <singhrounak00326@gmail.com>
2026-01-25 21:12:30 +0530

    Learned about If statement.

---
+++ conditions.c
@@ -0,0 +1,22 @@
+// Decision Making and Branching in C Programming.
+/*
+Decision making in C allows a program to execute different blocks of code depending on whether a condition is true or false. 
+Without this, your code would just run in a straight line from top to bottom every single time.
+
+1. The if Statement:
+The simplest form of branching. The code inside the braces runs only if the condition is true.
+*/
+// Example of If statement:
+#include<stdio.h>
+int main()
+{
+  int score;
+  printf("Enter your score");
+  scanf("%d", &score);
+  
+  if (score > 50) {
+    printf("You passed!");
+}
+  return 0;
+
+}
