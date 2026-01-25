7c58f920aa00fe6497750fd9284734001823e842
rounaksinghxd <singhrounak00326@gmail.com>
2026-01-25 21:15:34 +0530

    Learned If-else statement.

--- conditions.c
+++ conditions.c
@@ -5,17 +5,25 @@ Without this, your code would just run in a straight line from top to bottom eve
 
 1. The if Statement:
 The simplest form of branching. The code inside the braces runs only if the condition is true.
+
+2. The if-else Statement
+This provides an "Option B." If the if condition fails, the else block runs.
+
 */
 // Example of If statement:
 #include<stdio.h>
 int main()
 {
   int score;
-  printf("Enter your score");
+  printf("Enter your score: ");
   scanf("%d", &score);
   
   if (score > 50) {
     printf("You passed!");
+}
+//Example of If-else statement:
+  else {
+    printf("Failed");
 }
   return 0;
 
