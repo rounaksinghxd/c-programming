e1cd39f3a8f4b678004f969b74c5ce4fae324fbf
rounaksinghxd <singhrounak00326@gmail.com>
2026-02-12 21:42:01 +0530

    Learnbasics of goto statement

---
+++ goto.c
@@ -0,0 +1,4 @@
+/*
+The goto statement in C is a jump statement that allows you to transfer program control directly to another labeled part of the same function. 
+It is often referred to as an "unconditional jump."
+*/
