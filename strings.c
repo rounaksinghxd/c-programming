9fad154f95720a7b746a3baa84817b5bfa19dfc4
rounaksinghxd <singhrounak00326@gmail.com>
2026-08-09 13:36:36 +0530

    Used Accessing the string

--- strings.c
+++ strings.c
@@ -7,7 +7,16 @@ The compiler uses this null terminator to determine where the text actually ends
 
 #include<stdio.h>
 int main(){
-    char name[] ="Rounak"; 
+    char name[] ="Rounak"; /* This line declares a array name and intilize it with the string "Rounak".
+    Internally, this creates an array like: {'R', 'o', 'u', 'n', 'a', 'k', '\0'}
+The null character '\0' is automatically added at the end to terminate the string.
+*/
+
+
     printf("My Name is: %s\n", name); //%s is a type of format specifier which is used when we are working in String.
+    
+    //Accessing the characters: We can access the character same as we do in array we have to use index.
+    
+    printf("%c\n", name[5]); //this line of code is used to print the index number 5 of name string.
     return 0;
 }
