ecbaaa88b73ad7c9dd0e8e3533cefecc1ac9eec0
rounaksinghxd <singhrounak00326@gmail.com>
2026-08-18 02:00:37 +0530

    Define about function name and parenttheses

--- functions.c
+++ functions.c
@@ -4,7 +4,10 @@ In C programming, a function is a self-contained block of code that performs a s
 Every C program muA function is a block of code that performs a particular task and can be used whenever we need that task.
 */
 #include <stdio.h>
-void hello() // void is the retuen type. It means it doesn't return any value like int main().
+void hello() /*void is the retuen type. It means it doesn't return any value like int main().
+hello with the void is the function name.
+() These are called parentheses. They can contain information that we give to the function, called parameters.
+*/
 {
     /*In general we know if we have to print something we have to use printf, but let say if i want print 10 times or more than 10,
     Then we have to use functions in case of that*/
