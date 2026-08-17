d6bce0d6d2106de38d0b6497a2b657bf36673f53
rounaksinghxd <singhrounak00326@gmail.com>
2026-08-18 01:48:50 +0530

    Print Hello multiple times using functions

---
+++ functions.c
@@ -0,0 +1,20 @@
+// Functions in C Programming Language:
+/*
+In C programming, a function is a self-contained block of code that performs a specific task. 
+Every C program muA function is a block of code that performs a particular task and can be used whenever we need that task.
+*/
+#include <stdio.h>
+void hello() 
+{
+    printf("Hello\n");
+}
+int main() 
+{
+    hello();
+    hello();
+    hello();
+    hello();
+    return 0;
+}
+
+
